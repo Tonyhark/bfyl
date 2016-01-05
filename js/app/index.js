5f69ab0d19e1c8142055cc0bb8c0c18aeb10f5d7
Zhao Yue <tonyhark5483@hotmail.com>
2016-01-05 23:40:14 +0800

    增加解决方案-银行防爆页面

--- js/app/index.js
+++ js/app/index.js
@@ -11,4 +11,21 @@ seajs.use(['$', 'jquery/cycle/2.8.8/cycle'], function ($, cycle) {
             prev: '.slide-aow-l'
         });
     });
+
+    var $topBn = $('#top-banner');
+    var $topClose = $('.tb-close');
+    var topImg = $('<img src="img/index/top-banner.jpg"/>').on('load',function(e){
+
+        $topBn.slideDown();
+
+        setTimeout(function(){
+            $topBn.slideUp();
+        },10000);
+    });
+
+    $topClose.click(function(e){
+        $topBn.slideUp();
+    });
+
+
 });
